659d91589ac785d9eab4d191fb750a11dfd4ea84
Nitisha kushwah <nitishakushwah6@gmail.com>
2026-02-06 19:38:06 +0530

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -46,7 +46,7 @@ export const useAuthStore = create((set, get) => ({
   login: async (data) => {
     set({ isLoggingIn: true });
     try {
-      const res = await axiosInstance.post("api//auth/login", data);
+      const res = await axiosInstance.post("api/auth/login", data);
       set({ authUser: res.data });
       toast.success("Logged in successfully");
 
@@ -60,7 +60,7 @@ export const useAuthStore = create((set, get) => ({
 
   logout: async () => {
     try {
-      await axiosInstance.post("api//auth/logout");
+      await axiosInstance.post("api/auth/logout");
       set({ authUser: null });
       toast.success("Logged out successfully");
       get().disconnectSocket();
@@ -72,7 +72,7 @@ export const useAuthStore = create((set, get) => ({
   updateProfile: async (data) => {
     set({ isUpdatingProfile: true });
     try {
-      const res = await axiosInstance.put("api//auth/update-profile", data);
+      const res = await axiosInstance.put("api/auth/update-profile", data);
       set({ authUser: res.data });
       toast.success("Profile updated successfully");
     } catch (error) {
